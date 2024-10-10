67a9a225dc53e661f7f06e785e7265aef7c30c2c
Larry <larry@levart.com.au>
2024-10-10 16:14:37 +0800

    Quick script for removing a bunch of unneeded images

---
+++ rm_imgs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+docker rmi 01621a8a190a
+docker rmi 0839feeaa52b
+docker rmi 185afd510bc4
+docker rmi 2c41dafeb39f
+docker rmi 3efe89a438a9
+docker rmi 4590b8022b6f
+docker rmi 7f9513b2e7f3
+docker rmi 90d7653e6433
+docker rmi cf8e7d233ac0
